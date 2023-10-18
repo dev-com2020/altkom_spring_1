e48b155ee6fdd2263a73f6cba5f788d03e2fb8d1
dev-com2020 <kwp@tlen.pl>
2023-10-18 11:43:08 +0200

    11:42 18.10

--- demo/src/main/java/com/example/demo/HomeController.java
+++ demo/src/main/java/com/example/demo/HomeController.java
@@ -2,12 +2,23 @@ package com.example.demo;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class HomeController {
+    record Video(String name) {}
+    List<Video> videos = List.of(
+            new Video("Pierwsza aplikacja w Spring3"),
+            new Video("Jakiś drugi komunikat"),
+            new Video("To też powinno się wyświetlić")
+    );
+
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("video",videos);
         return "index";
     }
 }
