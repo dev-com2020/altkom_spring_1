63dd7bf6f652175f058dbc189da00bc8a108d1eb
dev-com2020 <kwp@tlen.pl>
2023-10-18 15:40:27 +0200

    15:40 18.10

--- demo/src/main/java/com/example/demo/HomeController.java
+++ demo/src/main/java/com/example/demo/HomeController.java
@@ -9,19 +9,25 @@ import org.springframework.web.bind.annotation.PostMapping;
 @Controller
 public class HomeController {
     private final VideoService videoService;
-    public HomeController(VideoService videoService){
+
+    public HomeController(VideoService videoService) {
         this.videoService = videoService;
     }
 
     @GetMapping("/")
     public String index(Model model) {
-        model.addAttribute("videos",videoService.getVideos());
+        model.addAttribute("videos", videoService.getVideos());
         return "index";
     }
 
     @PostMapping("/new-video")
-    public String newVideo(@ModelAttribute Video newVideo){
+    public String newVideo(@ModelAttribute Video newVideo) {
         videoService.create(newVideo);
         return "redirect:/";
+    }
+
+    @GetMapping("/react")
+    public String react() {
+        return "react";
     }
 }
