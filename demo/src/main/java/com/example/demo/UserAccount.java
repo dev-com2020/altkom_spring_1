7e66d955457d336ae42b90ad163772e9862dd072
dev-com2020 <kwp@tlen.pl>
2023-10-19 12:37:55 +0200

    12:37 19.10

--- demo/src/main/java/com/example/demo/UserAccount.java
+++ demo/src/main/java/com/example/demo/UserAccount.java
@@ -1,26 +1,36 @@
 package com.example.demo;
 
-import jakarta.persistence.ElementCollection;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
+@Entity
 public class UserAccount {
 
     @Id
     @GeneratedValue
     private Long id;
     private String username;
+
     private String password;
 
     @ElementCollection(fetch = FetchType.EAGER)
     private List<GrantedAuthority> authoritites = new ArrayList<>();
 
     protected UserAccount() {
+    }
+
+    public UserAccount(String username, String password, String... authoritites) {
+        this.username = username;
+        this.password = password;
+        this.authoritites = Arrays.stream(authoritites)
+                .map(SimpleGrantedAuthority::new)
+                .map(GrantedAuthority.class::cast)
+                .toList();
     }
 
 }
