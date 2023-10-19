aae098de3f2d668a097bec6b8729c988a6bcb8cf
dev-com2020 <kwp@tlen.pl>
2023-10-19 14:19:22 +0200

    14:19 19.10

--- demo/src/main/java/com/example/demo/HomeController.java
+++ demo/src/main/java/com/example/demo/HomeController.java
@@ -1,9 +1,11 @@
 package com.example.demo;
 
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
@@ -17,14 +19,15 @@ public class HomeController {
     }
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, Authentication authentication) {
         model.addAttribute("videos", videoService.getVideos());
+        model.addAttribute("authentication", authentication);
         return "index";
     }
 
     @PostMapping("/new-video")
-    public String newVideo(@ModelAttribute NewVideo newVideo) {
-        videoService.create(newVideo);
+    public String newVideo(@ModelAttribute NewVideo newVideo, Authentication authentication) {
+        videoService.create(newVideo, authentication.getName());
         return "redirect:/";
     }
 
@@ -35,17 +38,28 @@ public class HomeController {
 
     @PostMapping("/multi-field-search")
     public String multiFieldSearch(
-            @ModelAttribute VideoSearch search, Model model) {
+            @ModelAttribute VideoSearch search, Model model, Authentication authentication) {
         List<VideoEntity> searchResults = videoService.search(search);
         model.addAttribute("videos", searchResults);
+        model.addAttribute("search", search);
+        model.addAttribute("authentication", authentication);
         return "index";
     }
+
     @PostMapping("/universal-search")
     public String universalSearch(
-            @ModelAttribute UniversalSearch search, Model model) {
+            @ModelAttribute UniversalSearch search, Model model, Authentication authentication) {
         List<VideoEntity> searchResults = videoService.search(search);
         model.addAttribute("videos", searchResults);
+        model.addAttribute("search", search);
+        model.addAttribute("authentication", authentication);
         return "index";
+    }
+
+    @PostMapping("/delete/videos/{videoId}")
+    public String deleteVideo(@PathVariable Long videoId) {
+        videoService.delete(videoId);
+        return "redirect:/";
     }
 
 }
