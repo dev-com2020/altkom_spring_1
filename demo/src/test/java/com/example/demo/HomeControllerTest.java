6592f6124f383d13cc3a8a0eac38f9966cd3a46f
dev-com2020 <kwp@tlen.pl>
2023-10-20 11:24:53 +0200

    11:24 20.10

---
+++ demo/src/test/java/com/example/demo/HomeControllerTest.java
@@ -0,0 +1,57 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.containsString;
+import static org.mockito.Mockito.verify;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(controllers = HomeController.class)
+public class HomeControllerTest {
+
+    @Autowired
+    MockMvc mvc;
+    @MockBean VideoService videoService;
+    @Test
+    @WithMockUser
+    void indexPageHasSeveralHtmlForms() throws Exception {
+        String html = mvc.perform(
+                get("/"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("Username: user")))
+                .andExpect(content().string(containsString("Authorities: [ROLE_USER]")))
+                .andReturn()
+                .getResponse().getContentAsString();
+
+        assertThat(html).contains(
+                "<form action=\"/logout\"",
+                "<form action=\"/multi-field-search\"",
+                "<form action=\"/new-video\"");
+
+    }
+
+    @Test
+    @WithMockUser
+    void postNewVideoShouldWork() throws Exception {
+        mvc.perform(
+                post("/new-video")
+                        .param("name","new video")
+                        .param("description", "new desc")
+                        .with(csrf()))
+                .andExpect(redirectedUrl("/"));
+
+        verify(videoService).create(
+                new NewVideo("new video","new desc"), "user");
+
+    }
+
+}
